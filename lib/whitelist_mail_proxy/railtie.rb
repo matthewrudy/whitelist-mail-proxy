920c256e521cf6e8d25160934de463f83d18cb89
Matthew Rudy Jacobs <MatthewRudyJacobs@gmail.com>
2010-10-07 18:30:45 +0800

    only load the railtie if it hasnt already been loaded

--- lib/whitelist_mail_proxy/railtie.rb
+++ lib/whitelist_mail_proxy/railtie.rb
@@ -2,7 +2,7 @@ require 'rails'
 class WhitelistMailProxy
   class Railtie < Rails::Railtie
     initializer "actionmailer.whitelist" do
-      ActionMailer::Base.add_delivery_method(:whitelist_proxy, WhitelistMailProxy)
+      ActionMailer::Base.add_delivery_method(:whitelist_proxy, WhitelistMailProxy) unless ActionMailer::Base.respond_to?(:whitelist_proxy)
     end
   end
 end
